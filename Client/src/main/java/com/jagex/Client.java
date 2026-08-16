0d111b8947b4a4f246ee124e1006df10c479308e
Johto-Foundry <314530853+Johto-Foundry@users.noreply.github.com>
2026-08-16 02:44:25 +0000

    Remove synchronous console writes from rendering hot paths

--- Client/src/main/java/com/jagex/Client.java
+++ Client/src/main/java/com/jagex/Client.java
@@ -384,7 +384,6 @@ public final class Client implements Runnable {
 			if(System.currentTimeMillis() - lastMinimapUpdate > 100) {
 				lastMinimapUpdate = System.currentTimeMillis();
 				this.drawMinimapFullImage();
-				System.out.println("MINIMAP");
 				Chunk chunk = this.getCurrentChunk();
 				if(chunk != null) {
 					chunk.drawMinimapScene(Options.currentHeight.get());
@@ -1222,7 +1221,6 @@ public final class Client implements Runnable {
 	public void drawMinimapFullImage() {
 		if (!fullMapVisible.get())
 			return;
-		System.out.println("RENDER FULL MAP");
 		Platform.runLater(() -> {
 			for (Chunk chunk : chunks) {
 				try {
@@ -1567,7 +1565,6 @@ public final class Client implements Runnable {
 				Client.getSingleton().mapRegion.updateTiles();
 
 				SceneGraph.minimapUpdate = true;
-				System.out.println("UPDATED TILES");
 		});
 	}
 	
